924dad45aff93650da2c1e811c5f9f255454438d
dxvgef <dxvgef@outlook.com>
2019-04-08 00:28:55 +0800

    加入三个has验证
    hasLower,hasUpper,hasDigit,hasSpecialChar

--- rule/validator.go
+++ rule/validator.go
@@ -257,21 +257,25 @@ func chineseIdentityCard(str string) bool {
 	return false
 }
 
+// 是否存在小写字母
 func hasLower(str string) bool {
 	rule := `([a-z]+?)`
 	return regexp.MustCompile(rule).MatchString(str)
 }
 
+// 是否存在大写字母
 func hasUpper(str string) bool {
 	rule := `([A-Z]+?)`
 	return regexp.MustCompile(rule).MatchString(str)
 }
 
+// 是否存在数字
 func hasDigit(str string) bool {
 	rule := `(\d+?)`
 	return regexp.MustCompile(rule).MatchString(str)
 }
 
+// 是否存在特殊字符
 func hasSpecialChar(str string) bool {
 	rule := `([~!@#$%^&*()_+\-=:;\"'/?<>,.\[\]{}|]+?)`
 	return regexp.MustCompile(rule).MatchString(str)

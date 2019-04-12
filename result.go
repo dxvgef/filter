7c050626f7670383aac410aa8f51e5d07b9b51cf
dxvgef <dxvgef@outlook.com>
2019-04-13 01:12:50 +0800

    update

--- result.go
+++ result.go
@@ -2,34 +2,29 @@ package filter
 
 import (
 	"errors"
+	"log"
+	"reflect"
+	"strconv"
 )
 
-// filter 过滤器
+// filter 赋值过滤器
 type filter struct {
-	Value string
-	Rules []Rule
+	Pointer interface{}
+	Value   string
+	Rules   []Rule
 }
 
-// Filter 过滤器
-var Filter = func(value string, rules ...Rule) filter {
+// 过滤器
+var Filter = func(pointer interface{}, value string, rules ...Rule) filter {
 	return filter{
-		Value: value,
-		Rules: rules,
+		Pointer: pointer,
+		Value:   value,
+		Rules:   rules,
 	}
 }
 
-// GetAll 获得所有要过滤器的值
-func GetAll(f ...filter) error {
-	for k := range f {
-		if _, err := Get(f[k].Value, f[k].Rules...); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// Get 获得要过滤的值
-func Get(value string, rules ...Rule) (string, error) {
+// String 获得过滤后的字符串
+func String(value string, rules ...Rule) (string, error) {
 	// 运行修剪器
 	for k := range rules {
 		if rules[k].formatter != nil {
@@ -101,4 +96,84 @@ func Get(value string, rules ...Rule) (string, error) {
 
 	}
 	return value, nil
+}
+
+// Result 返回单个过滤和赋值的结果
+func Result(ptr interface{}, value string, rules ...Rule) error {
+	if ptr == nil {
+		return errors.New("参数不完整")
+	}
+	value, err := String(value, rules...)
+	if err != nil {
+		return err
+	}
+	return setPtr(ptr, value)
+}
+
+// AllResult 返回多个过滤和赋值的结果
+func AllResult(f ...filter) error {
+	for k := range f {
+		if err := Result(f[k].Pointer, f[k].Value, f[k].Rules...); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func setPtr(ptr interface{}, value string) error {
+	defer func() {
+		err := recover()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+	ptrOf := reflect.ValueOf(ptr)
+	if ptrOf.Kind() != reflect.Ptr {
+		return errors.New("必须传入" + ptrOf.Kind().String() + "的指针")
+	}
+
+	if ptrOf.Elem().CanSet() == false {
+		return errors.New("无法赋值到传入的变量")
+	}
+
+	ptrType := ptrOf.Elem().Type().Kind()
+	switch ptrType {
+	case reflect.String:
+		ptrOf.Elem().SetString(value)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return errors.New("无法将值转换为int类型")
+		}
+		if ptrOf.Elem().OverflowInt(v) == true {
+			return errors.New("无法赋值给传入的参数类型")
+		}
+		ptrOf.Elem().SetInt(v)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		v, err := strconv.ParseUint(value, 10, 64)
+		if err != nil {
+			return errors.New("无法将值转换为uint类型")
+		}
+		if ptrOf.Elem().OverflowUint(v) == true {
+			return errors.New("无法赋值给传入的参数类型")
+		}
+		ptrOf.Elem().SetUint(v)
+	case reflect.Float32, reflect.Float64:
+		v, err := strconv.ParseFloat(value, 10)
+		if err != nil {
+			return errors.New("无法将值转换为float类型")
+		}
+		if ptrOf.Elem().OverflowFloat(v) == true {
+			return errors.New("无法赋值给传入的参数类型")
+		}
+		ptrOf.Elem().SetFloat(v)
+	case reflect.Bool:
+		v, err := strconv.ParseBool(value)
+		if err != nil {
+			return errors.New("无法将值转换为bool类型")
+		}
+		ptrOf.Elem().SetBool(v)
+	}
+
+	return nil
 }

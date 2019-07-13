acdf23916f0287cc4267a611b743da185e387347
dxvgef <dxvgef@outlook.com>
2019-07-14 04:39:58 +0800

    加入Required()测试

--- basic_test.go
+++ basic_test.go
@@ -14,6 +14,31 @@ func TestBasic(t *testing.T) {
 	t.Log(password)
 }
 
+func TestSetRequired(t *testing.T) {
+	var username string
+	err := Set(&username, FromString("   ", "账号").
+		Trim().
+		Required("username不能是零值").
+		MinLength(6))
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	t.Log(username)
+}
+
+func TestRequired(t *testing.T) {
+	username, err := FromString("0", "账号").
+		Trim().
+		Required("username不能是零值").
+		MinInteger(1).Int64()
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	t.Log(username)
+}
+
 func BenchmarkBasic(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FromString("Abc123", "密码").

1a6ae38fa7fdb2b29af465724fbf5314b7c03a99
dxvgef <dxvgef@outlook.com>
2019-07-26 03:08:07 +0800

    增加Allow相关方法

--- slice.go
+++ slice.go
@@ -13,6 +13,127 @@ func (obj *Object) Separator(sep string) *Object {
 	return obj
 }
 
+// ------------------------------------ Allow ---------------------------------------
+
+// AllowStrings 仅允许[]string中的值
+func (obj *Object) AllowStrings(slice []string, customError ...string) *Object {
+	if obj.err != nil || obj.rawValue == "" {
+		return obj
+	}
+	for k := range slice {
+		if slice[k] != obj.rawValue {
+			obj.err = obj.setError("的值不在允许的范围", customError...)
+			return obj
+		}
+	}
+	return obj
+}
+
+// AllowInts 仅允许[]int中的值
+func (obj *Object) AllowInts(i []int, customError ...string) *Object {
+	if obj.err != nil || obj.rawValue == "" {
+		return obj
+	}
+	value, err := strconv.Atoi(obj.rawValue)
+	if err != nil {
+		obj.err = obj.setError(err.Error(), customError...)
+		return obj
+	}
+	for k := range i {
+		if value != i[k] {
+			obj.err = obj.setError("的值不在允许的范围", customError...)
+			return obj
+		}
+	}
+	return obj
+}
+
+// AllowInts32 仅允许[]int32中的值
+func (obj *Object) AllowInts32(i []int32, customError ...string) *Object {
+	if obj.err != nil || obj.rawValue == "" {
+		return obj
+	}
+	value64, err := strconv.ParseInt(obj.rawValue, 10, 32)
+	if err != nil {
+		obj.err = obj.setError(err.Error(), customError...)
+		return obj
+	}
+	value32 := int32(value64)
+	for k := range i {
+		if value32 != i[k] {
+			obj.err = obj.setError("的值不在允许的范围", customError...)
+			return obj
+		}
+	}
+	return obj
+}
+
+// AllowInts64 仅允许[]int64中的值
+func (obj *Object) AllowInts64(i []int64, customError ...string) *Object {
+	if obj.err != nil || obj.rawValue == "" {
+		return obj
+	}
+	if obj.i64 == 0 {
+		var err error
+		obj.i64, err = strconv.ParseInt(obj.rawValue, 10, 64)
+		if err != nil {
+			obj.err = obj.setError(err.Error(), customError...)
+			return obj
+		}
+	}
+	for k := range i {
+		if obj.i64 != i[k] {
+			obj.err = obj.setError("的值不在允许的范围", customError...)
+			return obj
+		}
+	}
+	return obj
+}
+
+// AllowFloats32 仅允许[]float32中的值
+func (obj *Object) AllowFloats32(f []float32, customError ...string) *Object {
+	if obj.err != nil || obj.rawValue == "" {
+		return obj
+	}
+	value, err := strconv.ParseFloat(obj.rawValue, 32)
+	if err != nil {
+		obj.err = obj.setError(err.Error(), customError...)
+		return obj
+	}
+	value32 := float32(value)
+	for k := range f {
+		if value32 != f[k] {
+			obj.err = obj.setError("的值不在允许的范围", customError...)
+			return obj
+		}
+	}
+	return obj
+}
+
+// AllowFloats64 仅允许[]float64中的值
+func (obj *Object) AllowInFloats64(f []float64, customError ...string) *Object {
+	if obj.err != nil || obj.rawValue == "" {
+		return obj
+	}
+	if obj.f64 == 0 {
+		var err error
+		obj.f64, err = strconv.ParseFloat(obj.rawValue, 64)
+		if err != nil {
+			obj.err = obj.setError(err.Error(), customError...)
+			return obj
+		}
+	}
+	for k := range f {
+		if obj.f64 != f[k] {
+			obj.err = obj.setError("的值被阻止", customError...)
+			return obj
+		}
+	}
+	return obj
+}
+
+// ------------------------------------ Deny ---------------------------------------
+
 // DenyStrings 阻止存在于[]string中的值
 func (obj *Object) DenyStrings(slice []string, customError ...string) *Object {
 	if obj.err != nil || obj.rawValue == "" {
@@ -20,7 +141,7 @@ func (obj *Object) DenyStrings(slice []string, customError ...string) *Object {
 	}
 	for k := range slice {
 		if slice[k] == obj.rawValue {
-			obj.err = obj.setError("不允许的值", customError...)
+			obj.err = obj.setError("的值被阻止", customError...)
 			return obj
 		}
 	}
@@ -39,7 +160,7 @@ func (obj *Object) DenyInts(i []int, customError ...string) *Object {
 	}
 	for k := range i {
 		if value == i[k] {
-			obj.err = obj.setError("不允许的值", customError...)
+			obj.err = obj.setError("的值被阻止", customError...)
 			return obj
 		}
 	}
@@ -59,7 +180,7 @@ func (obj *Object) DenyInts32(i []int32, customError ...string) *Object {
 	value32 := int32(value64)
 	for k := range i {
 		if value32 == i[k] {
-			obj.err = obj.setError("不允许的值", customError...)
+			obj.err = obj.setError("的值被阻止", customError...)
 			return obj
 		}
 	}
@@ -81,7 +202,7 @@ func (obj *Object) DenyInts64(i []int64, customError ...string) *Object {
 	}
 	for k := range i {
 		if obj.i64 == i[k] {
-			obj.err = obj.setError("不允许的值", customError...)
+			obj.err = obj.setError("的值被阻止", customError...)
 			return obj
 		}
 	}
@@ -101,7 +222,7 @@ func (obj *Object) DenyFloats32(f []float32, customError ...string) *Object {
 	value32 := float32(value)
 	for k := range f {
 		if value32 == f[k] {
-			obj.err = obj.setError("不允许的值", customError...)
+			obj.err = obj.setError("的值被阻止", customError...)
 			return obj
 		}
 	}
@@ -123,7 +244,7 @@ func (obj *Object) DenyInFloats64(f []float64, customError ...string) *Object {
 	}
 	for k := range f {
 		if obj.f64 == f[k] {
-			obj.err = obj.setError("不允许的值", customError...)
+			obj.err = obj.setError("的值被阻止", customError...)
 			return obj
 		}
 	}

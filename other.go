489f3029ab62f2ee91873c52fb41767d93aa258f
dxvgef <dxvgef@outlook.com>
2020-05-23 21:31:45 +0800

    增加方法：IsURLQuery

--- other.go
+++ other.go
@@ -19,3 +19,12 @@ func (obj *Object) IsRequestURI(customError ...string) *Object {
 	_, obj.err = url.ParseRequestURI(obj.rawValue)
 	return obj
 }
+
+// 是有效的URL Query
+func (obj *Object) IsURLQuery(customError ...string) *Object {
+	if obj.err != nil || obj.rawValue == "" {
+		return obj
+	}
+	_, obj.err = url.ParseQuery(obj.rawValue)
+	return obj
+}

9907286a5481d63e22728ab765e3c7a0c5123380
dxvgef <dxvgef@outlook.com>
2020-06-26 10:01:15 +0800

    update

--- example_test.go
+++ example_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // 检查过滤结果
 func TestCheck(t *testing.T) {
-	err := String("dxvgef", Config{Name: "账号"}).
+	err := String("dxvgef", Config{Name: "账号", Require: true}).
 		TrimSpace().
 		MinLength(3, "不能少于3个字符").MaxLength(15).
 		Error()
@@ -16,7 +16,7 @@ func TestCheck(t *testing.T) {
 // 将过滤结果赋值到变量
 func TestSet(t *testing.T) {
 	var str string
-	err := String("dxvgef", Config{Name: "账号"}).
+	err := String("dxvgef", Config{Name: "账号", Require: true}).
 		TrimSpace().
 		MinLength(3, "不能少于3个字符").MaxLength(15).
 		Set(&str)
@@ -29,7 +29,7 @@ func TestSet(t *testing.T) {
 
 // 类型转换
 func TestConvert(t *testing.T) {
-	i, err := String("18", Config{Name: "年龄"}).
+	i, err := String("18", Config{Name: "年龄", Require: true}).
 		TrimSpace().
 		MinLength(18, "不能小于18岁").
 		Int()
@@ -43,8 +43,10 @@ func TestConvert(t *testing.T) {
 // 批量处理检查结果
 func TestBatchCheck(t *testing.T) {
 	err := Batch(
-		String("dxvgef", Config{Name: "账号"}).MinLength(3, "不能少于3个字符").MaxLength(15).Error(),
-		String("123456", Config{Name: "密码"}).MinLength(6, "不能少于6个字符").MaxLength(15).Error(),
+		String("dxvgef", Config{Name: "账号", Require: true}).
+			MinLength(3, "不能少于3个字符").MaxLength(15).Error(),
+		String("123456", Config{Name: "密码", Require: true}).
+			MinLength(6, "不能少于6个字符").MaxLength(15).Error(),
 	)
 	if err != nil {
 		t.Error(err)
@@ -55,8 +57,12 @@ func TestBatchCheck(t *testing.T) {
 func TestBatchSet(t *testing.T) {
 	var str1, str2 string
 	err := Batch(
-		String("dxvgef", Config{Name: "账号"}).MinLength(3, "不能少于3个字符").MaxLength(15).Error(),
-		String("123456", Config{Name: "密码"}).MinLength(6, "不能少于6个字符").MaxLength(15).Error(),
+		String("dxvgef", Config{Name: "账号", Require: true}).
+			MinLength(3, "不能少于3个字符").MaxLength(15).
+			Set(&str1),
+		String("123456", Config{Name: "密码", Require: true}).
+			MinLength(6, "不能少于6个字符").MaxLength(15).
+			Set(&str2),
 	)
 	if err != nil {
 		t.Error(err)
